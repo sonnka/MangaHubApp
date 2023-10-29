35c1af5c5ee38f5013fd28b8a94f5b1552977afe
sonnka <sonyakazanceva1331@gmail.com>
2023-10-30 00:49:28 +0200

    Added registration and main forms

---
+++ app/src/main/java/com/manga/mangahubapp/activity/RegisterPage.kt
@@ -0,0 +1,12 @@
+package com.manga.mangahubapp.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.manga.mangahubapp.R
+
+class RegisterPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
